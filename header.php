ba75f901af1f065b6ef4747b52f7c5fcebbe1bb8
Hugo Marx <marx.hugo@gmail.com>
2022-03-23 14:53:51 +0100

    navbar logo change

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <img src="img/logo.svg" alt="logo batman" />
+  <img src="img/batman-logo-inverted.png" alt="logo batman" style=" width:20%; margin: 2vw;"  />
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
