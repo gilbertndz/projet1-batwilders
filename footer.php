31bf3cf1a5cf30603deadd323be3a33a9c290049
Harxia <jonathan.matulla@gmail.com>
2022-03-23 14:47:51 +0100

    Add Footer PHP

---
+++ footer.php
@@ -0,0 +1,67 @@
+ <?php
+    echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">';
+    echo '<link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"
+      integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />';
+    echo '<link rel="stylesheet" href="css/footer.css">';
+
+    echo '<!-- Site footer -->
+ <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>A propos</h6>
+            <p class="text-justify">Portfolio <i> Site web - BATMAN</i></p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Services</h6>
+            <ul class="footer-links">
+              <li><a href="">Bat Rescue</a></li>
+              <li><a href="">Bat Ride</a></li>
+              <li><a href="">Super Bat duo</a></li>
+              <li><a href="">Bat Fun</a></li>
+              <li><a href=""></a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Liens rapides</h6>
+            <ul class="footer-links">
+              <li><a href="">Acceuil</a></li>
+              <li><a href="">A propos</a></li>
+              <li><a href="">Services</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2022 Batman Inc.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#" style="display: flex; justify-content: center; align-items:center"><i class="fa-brands fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#" style="display: flex; justify-content: center; align-items:center"><i class="fa-brands fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#" style="display: flex; justify-content: center; align-items:center"><i class="fa-brands fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#" style="display: flex; justify-content: center; align-items:center"><i class="fa-brands fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>';
+
+    echo '
+ <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+ ';
