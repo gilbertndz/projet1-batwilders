3df0f81970c99f564c51face88409aa5c7751fe2
Hugo Marx <marx.hugo@gmail.com>
2022-03-23 14:08:52 +0100

    update vilain section + avis_clients.php

---
+++ about_data.php
@@ -0,0 +1,8 @@
+<?php
+$vilain = [
+    ['name' => 'Joker', 'prison_date' => '01-01-1998'],
+    ['name' => 'Mr. Freeze', 'prison_date' => '01-01-1978'],
+    ['name' => 'Ban', 'prison_date' => '01-01-2008'],
+    ['name' => 'Poison Ivy', 'prison_date' => '01-01-2017'],
+
+];
---
+++ avis_client.php
@@ -0,0 +1,47 @@
+<!-- Carrousel avis clients -->
+
+<div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel" data-bs-interval="10000" >
+  <div class="carousel-inner container-sm mt-5 mb-5">
+    <div class="carousel-item active p-3 text-center">
+      <p class="w-75 m-auto  text-light"> <span>"</span> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias dolorum voluptas facere sed totam possimus fugiat voluptatem.<span>"</span></p>
+      <h3>Patrick, Gotham City</h3>
+      <div class="rating">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div class="empty"></div>
+                </div>
+    </div>
+    <div class="carousel-item p-3 text-center">
+    <p class="w-75 m-auto  text-light"><span>"</span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias dolorum voluptas facere sed totam possimus fugiat voluptatem.<span>"</span></p>
+    <h3>Julie, Rathsamhausen</h3>
+    <div class="rating">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+    </div>
+    <div class="carousel-item p-3 text-center">
+    <p class="w-75 m-auto  text-light"><span>"</span> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias dolorum voluptas facere sed totam possimus fugiat voluptatem.<span>"</span></p>
+    <h3>Alfred, Gotham City</h3>
+    <div class="rating">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
