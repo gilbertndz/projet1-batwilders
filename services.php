994951a5635ff29d100adb17e8b05584db1d031a
gilbertndz <gilbert.ndayizeye@hotmail.fr>
2022-03-25 09:41:12 +0100

    Page Services ok

--- services.php
+++ services.php
@@ -3,10 +3,10 @@
 echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">';
 echo '<link href="/css/services.css" rel="stylesheet"';
 echo '<link href="/css/about_bts.css" rel="stylesheet" type="text/css">';
-echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-';
+echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>';
 
 include ('./header.php') ;
+include ('./services_data.php');
 
 echo '<br/>';
 
@@ -28,7 +28,16 @@ echo '<div id="service" class="container mt-3 mb-5">';
                         <div class="card-body">
                           <h5 class="card-title">'.$value['title'].'</h5>
                           <p class="card-text">'.$value['description'].'</p>
-                          <a href="#" class="btn btn-dark">'.$value['button'].'</a>
+                            <div class="popover__wrapper">
+                            <a href="#">
+                            <h2 class="popover__title">'.$value['button'].'</h2>
+                            </a>
+                            <div class="popover__content">
+                            <h3 class="popover__message">'.$value['title'].'</h3>
+                            <br/> 
+                            <p class="popover__message">'.$value['pop_message'].'</p>
+                            </div>
+                            </div>
                         </div>
                       </div>
                     </div>';
--- services_data.php
+++ services_data.php
@@ -3,31 +3,55 @@
 
 $data = [
 
-'Mes Bat\'services' => [
+'Mes Bat\' Services' => [
         [
             'button' => 'En savoir plus',
             'title' => 'Bat\' Rescue',
             'description' => 'Une urgence ? un imprévu ? Une menace ?
             Le service Bat\'rescue est là pour répondre à une multitude de besoins.',
             'image' => 'rescue.jpg',
+            'pop_message' => '<ul>
+            <li>Vengeance contre un méchant <br/> Délai d\'intervention entre 24 et 48h</li> <hr/>
+            <li>Enquête sur vos ennemis <br/> Délai d\'intervention variable selon le type d\'enquête</li><hr/>
+            <li>Assitance suite à un vol <br/> Délai d\'invertention inférieur à 12h</li>
+            </ul><hr/><br/>
+            Vous pouvez obtenir un devis ou réserver votre Bat\' Service en <a href="/contact.php">cliquant ici</a>',
         ],
         [
             'button' => 'En savoir plus',
             'title' => 'Bat\' Ride',
             'description' => 'Un lieu qui vous fait rêver ou un rendez-vous important ? Une envie de vous évader ? La Bat\'mobile vous conduira où vous souhaitez en un temps record !',
             'image' => 'ride.jpg',
+            'pop_message' => '<ul> 
+            <li>Vengeance contre un méchant <br/> Délai d\'intervention entre 24 et 48h</li> <hr/>
+            <li>Enquête sur vos ennemis <br/> Délai d\'intervention variable selon le type d\'enquête</li><hr/>
+            <li>Assitance suite à un vol <br/> Délai d\'invertention inférieur à 12h</li>
+            </ul><hr/><br/>
+            Vous pouvez obtenir un devis ou réserver votre Bat\' Service en <a href="/contact.php">cliquant ici</a>',
         ],
         [
             'button' => 'En savoir plus',
-            'title' => 'Super\'Bat\' Duo',
+            'title' => 'Super\' Bat\' Duo',
             'description' => 'Superman et Batman s\'associent pour vous offrir un service optimal !',
             'image' => 'superman.jpg',
+            'pop_message' => '<ul>
+            <li>Vengeance contre un méchant <br/> Délai d\'intervention entre 24 et 48h</li> <hr/>
+            <li>Enquête sur vos ennemis <br/> Délai d\'intervention variable selon le type d\'enquête</li><hr/>
+            <li>Assitance suite à un vol <br/> Délai d\'invertention inférieur à 12h</li>
+            </ul><hr/><br/>
+            Vous pouvez obtenir un devis ou réserver votre Bat\' Service en <a href="/contact.php">cliquant ici</a>',
         ],
         [
             'button' => 'En savoir plus',
             'title' => 'Bat\' Fun',
             'description' => 'Besoin de passer du bon temps et vous détendre ? Je vous accompagne dans tous vos loisirs et aventures. Si vous cherchez un compagnon pour rompre la solitude, vous êtes au bon endroit !',
             'image' => 'fun.jfif',
+            'pop_message' => '<ul>
+            <li>Vengeance contre un méchant <br/> Délai d\'intervention entre 24 et 48h</li> <hr/>
+            <li>Enquête sur vos ennemis <br/> Délai d\'intervention variable selon le type d\'enquête</li><hr/>
+            <li>Assitance suite à un vol <br/> Délai d\'invertention inférieur à 12h</li>
+            </ul><hr/><br/>
+            Vous pouvez obtenir un devis et réserver votre Bat\' Service en <a href="/contact.php">cliquant ici</a>',
         ],
     ],
 ];
